19935d1e001bdb30668e1f072959324feecb59eb
apreziosi1 <a.preziosi@outlook.it>
2023-06-28 17:17:01 +0200

    added card selection

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import MovieCard from './components/MovieCard';
 function App() {
 
   const [movies, setMovies] = useState ([]);
+  const [selectedMovie, setSelectedMovie] = useState([]);
   
   useEffect(() => {
     fetchMovies();
@@ -14,6 +15,7 @@ function App() {
     const movies = await data.json();
     console.log(movies)
     setMovies(movies.results)
+    setSelectedMovie(movies.results[0])
   }
 
   const [searchTerm, setTerm] = useState ('');
@@ -24,36 +26,44 @@ function App() {
         const searchedMovies = await data.json();
         console.log(searchedMovies)
         setMovies(searchedMovies.results)
+        setSelectedMovie(searchedMovies.results[0])
     }
 
     const handleChange = (e) => {
         setTerm(e.target.value)
     }
 
+   
+
 
   return (
-    <>
     <div className='p-8 flex flex-col justify-center'>
       <h1 className='font-bold text-6xl text-center'>Movie Database</h1>
-      <div className="flex flex-col my-8 items-center gap-4">
-      <div className="container flex justify-center">
+      <div className='flex flex-col my-8 items-center gap-4'>
+      <div className='container flex justify-center'>
         <form action="" onSubmit={handleSubmit} className="w-full flex justify-center items-center gap-8">
-          <input className="w-full h-[36px] rounded-full text-center text-lg" type="text" onChange={handleChange}/>
-          <button className="min-w-[100px] p-2 rounded-xl hover:bg-black hover:text-white border-2 border-black ">Search</button> 
+          <input className='w-full h-[36px] rounded-full text-center text-lg" type="text' onChange={handleChange}/>
+          <button className='min-w-[100px] p-2 rounded-xl hover:bg-black hover:text-white border-2 border-black'>Search</button> 
         </form>
-        <button className="mx-2 min-w-[100px] p-2 rounded-xl hover:bg-black hover:text-white border-2 border-black" onClick={fetchMovies} >Reset</button>
+        <button className='mx-2 min-w-[100px] p-2 rounded-xl hover:bg-black hover:text-white border-2 border-black' onClick={fetchMovies} >Reset</button>
       </div>
-      
+      <div className='rounded-2xl shadow-2xl mt-16 mb-8 flex justify-end bg-top-center bg-cover w-full h-[720px]' style={{backgroundImage: `url(${process.env.REACT_APP_IMG_PATH}${selectedMovie.backdrop_path}`}}>
+        <div className='p-16 text-right flex flex-col justify-center items-end w-1/4 bg-gradient-to-r from-transparent to-black text-white rounded-2xl'>
+          <h1 className='text-6xl mb-6'>{selectedMovie.title}</h1>
+          {selectedMovie.overview ? <p>{selectedMovie.overview}</p> : null}
+          <button className='mt-16 max-w-[100px] p-2 rounded-xl hover:scale-125 duration-300 border-2'>Play trailer</button> 
+        </div>
+        
       </div>
-      <div className='p-8 flex flex-wrap justify-center gap-3'>
+      </div>
+      <div className='flex flex-wrap justify-center gap-3'>
       {movies.map(movie => {
         return (
-          <MovieCard key={movie.id} movie={movie}/>
+          <MovieCard key={movie.id} movie={movie} selectMovie={setSelectedMovie}/>
         )
       })}
     </div>
    </div>
-   </>
   );
 }
 
--- src/components/MovieCard.js
+++ src/components/MovieCard.js
@@ -1,6 +1,6 @@
-const MovieCard = ({movie}) => {
+const MovieCard = ({movie, selectMovie}) => {
     return (
-        <div className="flex flex-col max-w-[400px] rounded-b-2xl justify-center shadow-xl bg-[#E0E1DD]">
+        <div className="flex flex-col max-w-[400px] rounded-b-2xl justify-center shadow-xl bg-[#E0E1DD]" onClick={() => selectMovie(movie)}>
             <div className="overflow-hidden cursor-pointer relative rounded-t-2xl group">
                 {movie.poster_path !== null
                 ? <img alt={movie.title} className="rounded-t-2xl max-h-[800px] max-w-[400px] object-cover h-full w-full group-hover:scale-110 duration-300 ease-in-out" src={"https://image.tmdb.org/t/p/w500" + movie.poster_path} />
